efbb5042bb6559f9f6bde85612e1231e3dcc2a86
[React1] 김선규 <seongyu1066@naver.com>
2024-12-12 16:55:20 +0900

    edit rootRouter

--- src/rootRouter.ts
+++ src/rootRouter.ts
@@ -3,7 +3,9 @@ import { Test } from "./sequelize";
 
 const rootRouter = Router();
 
-rootRouter.get("/", (req: Request, res: Response) => res.send("김선규"));
+rootRouter.get("/", (req: Request, res: Response) =>
+  res.send("김선규!123123@@@@")
+);
 
 rootRouter.get("/content", async (req: Request, res: Response) => {
   const tests = await Test.findAll({});
